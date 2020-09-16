195ecc48da2f5355119327a8d57070d896859d8a
kaytharimyatmoe <kaytharimyatmoe@gmail.com>
2020-09-16 09:39:54 +0630

    another try

--- app/src/main/java/com/rajat/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/rajat/pdfviewer/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
                     this,
                     url,
                     false,
+                    "",
                     "title",
                     ""
                 )
